7800f7a8aebabac1dcb596cbc247435bf9be4149
TuesdayJZ <moka010302@gmail.com>
2022-08-11 23:35:19 +0900

    四則演算が可能

--- 9cc.c
+++ 9cc.c
@@ -5,6 +5,8 @@
 #include<stdbool.h>
 #include<string.h>
 
+/* tokenizer */
+
 //トークンの種類
 typedef enum {
     TK_RESERVED, //記号
@@ -26,7 +28,16 @@ struct Token {
 char *user_input; //入力プログラム
 Token *token;  //現在のトークン linked list になる
 
-//改良版・エラーをprintするための関数
+//エラーを報告するための関数
+void error(char *fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+  vfprintf(stderr, fmt, ap);
+  fprintf(stderr, "\n");
+  exit(1);
+}
+
+//改良版・エラーを報告するための関数
 void error_at(char *location, char *fmt, ...) {
   va_list ap;
   va_start(ap, fmt);
@@ -87,8 +98,8 @@ Token *tokenize(char *p) {
       continue;
     }
 
-    //+か=だったら新しいトークンを作ってcurrentに繋げる
-    if (*p == '+' || *p == '-') {
+    //数字以外だったら新しいトークンを作ってcurrentに繋げる
+    if (strchr("+-*()/", *p)) {
       current = new_token(TK_RESERVED, current, p++);
       continue;
     }
@@ -100,47 +111,140 @@ Token *tokenize(char *p) {
       continue;
     }
 
-    error_at(p, "数字ではありません");
+    error_at(p, "無効なトークンです");
   }
 
   new_token(TK_EOF, current, p);
   return head.next;
 }
 
-int main(int argc, char **argv) {
-  if (argc != 2) {
-    fprintf(stderr, "引数の個数が正しくありません\n");
-    return 1;
+/* parser */
+
+//抽象構文木のノードの種類
+typedef enum {
+  ND_ADD,
+  ND_SUB,
+  ND_MUL,
+  ND_DIV,
+  ND_NUM,
+} NodeKind;
+
+typedef struct Node Node;
+
+//抽象構文木のノードの型
+struct Node {
+  NodeKind kind;    //ノードの型
+  Node *leftside;   //左辺
+  Node *rightside;  //右辺
+  int val;          // ND_NUMの場合その値
+};
+
+//新しいノードを作成 数字以外の場合
+Node *new_node(NodeKind kind, Node *leftside, Node *rightside){
+  Node *node = calloc(1, sizeof(Node));
+  node->kind = kind;
+  node->leftside = leftside;
+  node->rightside = rightside;
+  return node;
+}
+
+//新しいノードを作成 数字の場合
+Node *new_node_num(int val) {
+  Node *node = calloc(1, sizeof(Node));
+  node->kind = ND_NUM;
+  node->val = val;
+  return node;
+}
+
+Node *expr();
+Node *mul();
+Node *primary();
+
+Node *expr() {
+  Node *node = mul();
+  for (;;) {
+    if (consume('+'))
+      node = new_node(ND_ADD, node, mul());
+    else if (consume('-'))
+      node = new_node(ND_SUB, node, mul());
+    else
+      return node;
   }
+}
+
+Node *mul() {
+  Node *node = primary();
+  for (;;) {
+    if (consume('*'))
+      node = new_node(ND_MUL, node, primary());
+    else if (consume('/'))
+      node = new_node(ND_DIV, node, primary());
+    else
+      return node;
+  }
+}
+
+Node *primary(){
+  //()の中に入っているのはexprのはず
+  if(consume('(')){
+    Node *node = expr();
+    expect(')');
+    return node;
+  }
+  //そうでなかったら数字
+  return new_node_num(expect_number());
+}
+
+//アセンブリを出力
+void gen(Node *node){
+  if (node->kind == ND_NUM) {
+    printf("  push %d\n", node->val);
+    return;
+  }
+
+  gen(node->leftside);
+  gen(node->rightside);
+
+  printf("  pop rdi\n");
+  printf("  pop rax\n");
+
+  switch (node->kind) {
+    case ND_ADD:
+      printf("  add rax, rdi\n");
+      break;
+    case ND_SUB:
+      printf("  sub rax, rdi\n");
+      break;
+    case ND_MUL:
+      printf("  imul rax, rdi\n");
+      break;
+    case ND_DIV:
+      printf("  cqo\n");
+      printf("  idiv rdi\n");
+      break;
+  }
+
+  printf("  push rax\n");
+}
+
+int main(int argc, char **argv) {
+  if (argc != 2) error("%s: 引数の個数が正しくありません\n", argv[0]);
 
   //入力文字列をトークナイズする
   user_input = argv[1];
   token = tokenize(user_input);
+  Node *node = expr();
 
   //アセンブリの冒頭部分を出力
   printf(".intel_syntax noprefix\n");
   printf(".global main\n");
   printf("main:\n");
 
-  //式の最初が数字になっているかチェックしてmov命令を出力
-  printf("  mov rax, %d\n", expect_number());
+  //抽象構文木をアセンブリに変換して出力
+  gen(node);
 
-  // '+ <数>' または '- <数>' の並びを消費しつつアセンブリを出力
-  while(!at_eof()){
-    if(consume('+')){
-      printf("  add rax, %d\n", expect_number());
-      continue;
-    }
-
-    /*if(consume('-')){
-      printf("  sub rax, %d\n", expect_number());
-      continue;
-    }*/
-
-    expect('-');
-    printf("  sub rax, %d\n", expect_number());
-  }
-
+  //アセンブリの後半部分を出力
+  printf("  pop rax\n");
   printf("  ret\n");
   return 0;
 }
